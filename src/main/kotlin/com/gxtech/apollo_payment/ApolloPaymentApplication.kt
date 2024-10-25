22791cb0a0b8ba0f6fdb12d133208ff0a29b9b81
gus-chinaglia <gustavochinaglia.dev@gmail.com>
2024-10-25 11:49:04 -0300

    init

---
+++ src/main/kotlin/com/gxtech/apollo_payment/ApolloPaymentApplication.kt
@@ -0,0 +1,11 @@
+package com.gxtech.apollo_payment
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApolloPaymentApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApolloPaymentApplication>(*args)
+}
